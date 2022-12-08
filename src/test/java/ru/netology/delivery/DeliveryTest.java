0db74e7ed2c2a2faba6c85023b357ff9d6dc76f2
anastasiynovaya <aefremova3014@gmail.com>
2022-12-08 10:09:44 +0500

    reporting

--- src/test/java/ru/netology/delivery/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/DeliveryTest.java
@@ -1,8 +1,8 @@
 package ru.netology.delivery;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
@@ -13,6 +13,14 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 class DeliveryTest {
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     void setup() {
